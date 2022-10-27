21b0a35611896f1ea5cfcb3abcdfd0e261942971
brzozaKoduje <p.sobecki@op.pl>
2022-10-27 17:37:30 +0200

    Zad 1 Zad 2

---
+++ src/main/java/zadania/Zadanie1.java
@@ -0,0 +1,33 @@
+package zadania;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+// Javadoc /**
+/**
+ * Stwórz metodę, która jako parametr przyjmuje listę stringów, następnie zwraca tą
+ * listę posortowaną alfabetycznie od Z do A.
+ */
+public class Zadanie1 {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("aaaabbb");
+        list.add("AAAAbbb");
+
+        System.out.println("Bez sortu: " + list);
+
+        Zadanie1.sortList(list);
+
+        System.out.println("Sortowana: " + list);
+
+    }
+
+
+    public static List<String> sortList(List<String> list){
+        Collections.sort(list);
+//        list.sort(Comparator.reverseOrder());
+        return list;
+    }
+}
